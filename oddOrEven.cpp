3899eaa555db1cafda6e14f119b3aebb2cc0ae18
Shashikant Yadav <112337020+shashican17@users.noreply.github.com>
2025-02-25 14:10:51 +0530

    Add odd or even code.

---
+++ oddOrEven.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void oddOrEven(int a){
+    int b = 1;
+
+    if(a&b == 1){
+        cout << "odd\n";
+    }else{
+        cout << "even\n";
+    }
+}
+
